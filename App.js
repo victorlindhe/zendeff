52a499b21246252ccb1f544bdf11581aa448ca8d
Victor Lindhé <victorlindhe@gmail.com>
2018-04-07 13:59:43 +0200

    Add Settings page component

--- App.js
+++ App.js
@@ -1,23 +1,26 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StackNavigator } from 'react-navigation';
+import Settings from 'zendeff/components/settings'
 
+/*
+ * Application entry point. Checks for settings. If none set, then redirect to settings page.
+ */
+const RootStack = StackNavigator(
+  {
+    Settings: {
+      screen: Settings
+    }
+  },
+  {
+    initialRouteName: 'Settings'
+  }
+);
+
+/*
+ * App with RootStack element
+ */
 export default class App extends React.Component {
   render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
+    return <RootStack />
   }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+};
---
+++ components/settings/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+/*
+ * Page for application settings
+ */
+export default class Settings extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return(
+      <View>
+        <Text>Settings page</Text>
+      </View>
+    );
+  }
+}
