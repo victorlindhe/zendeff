a8f31e770e6bdf7e0f30acda847b860857b3cc05
Victor Lindhé <victorlindhe@gmail.com>
2018-04-07 16:34:49 +0200

    Change colors to app

--- config/globals.js
+++ config/globals.js
@@ -2,8 +2,8 @@
  *  Global variables
  */
 export default {
-  PRIMARY_COLOR: '#009688',
-  SECONDARY_COLOR: '#BF360C',
+  PRIMARY_COLOR: '#3F51B5',
+  SECONDARY_COLOR: '#E65100',
   DEFAULT_SETTINGS: {
     gender: 'male',
     interval: 7
