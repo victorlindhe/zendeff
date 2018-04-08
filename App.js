f6101412dd4651224c24262ba2549c090f828020
Victor Lindhé <victorlindhe@gmail.com>
2018-04-08 16:00:27 +0200

    Add history component for listing entries

--- App.js
+++ App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { StackNavigator, SwitchNavigator, TabNavigator } from 'react-navigation';
 import Settings from 'zendeff/components/settings';
 import Today from 'zendeff/components/today';
+import History from 'zendeff/components/history';
 
 /*
  * Application entry point.
  */
 const AppStack = TabNavigator({ 
   Today: StackNavigator({ Today: Today }), 
+  History: StackNavigator({ History: History }),
   Settings: StackNavigator({ Settings: Settings })
 });
 
---
+++ components/history/index.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import { View, Text, StyleSheet, Button, TextInput, AsyncStorage, SafeAreaView } from 'react-native';
+import GlobalStyles from 'zendeff/config/styles.js';
+import Globals from 'zendeff/config/globals.js';
+import ColoredButton from 'zendeff/components/coloredButton';
+import WeightForm from 'zendeff/components/weightForm';
+import Stats from 'zendeff/components/stats';
+import WeightEntriesCollection from 'zendeff/domains/weightEntries/WeightEntriesCollection.js';
+
+const styles = StyleSheet.create(GlobalStyles);
+
+/*
+ * Displays list of current data, last first
+ */
+export default class History extends React.Component {
+  static navigationOptions = {
+    title: 'History',
+    headerStyle: GlobalStyles.primaryBackground,
+    headerTintColor: '#fff',
+    headerRight: (
+      <Button
+        onPress={() => alert('hello')}
+        title='Add day'
+        color="#fff"/>
+    )
+  }
+
+  constructor(props) {
+    super(props);
+    this.state = {};
+    this._load();
+  }
+
+  /*
+   * Loads stored weightEntries
+   */
+  _load = async () => {
+    const weightEntries = await AsyncStorage.getItem(Globals.DATA_ENTRIES);
+    let weightEntriesCollection = new WeightEntriesCollection(weightEntries);
+
+    this.setState({
+      weightEntries: weightEntriesCollection
+    });
+  }
+
+  /* 
+   * Renders list of entries
+   */
+  render() {
+    return null
+  }
+}
