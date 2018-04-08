418197f80a0912d11fc4efb3d984a46f512105a2
Victor Lindhé <victorlindhe@gmail.com>
2018-04-08 15:26:29 +0200

    Add average method to WeightEntriesCollection

--- domains/weightEntries/WeightEntriesCollection.js
+++ domains/weightEntries/WeightEntriesCollection.js
@@ -47,13 +47,28 @@ export default class WeightEntriesCollection {
   /*
    * Sorts entries by date
    */
-  getSorted() {
+  getSorted(inverse = false) {
     return this.entries.sort((e1, e2) => {
       let e1time = e1.date.getTime(), e2time = e2.date.getTime();
+      const i = inverse ? -1 : 1;
 
       if(e1time === e2time) return 0;
-      return e1time > e2time ? 1 : -1;
+      return e1time > e2time ? i*1 : i*-1;
     });
+  }
+
+  /*
+   * Returns average of last N days, for key K
+   */
+  getAverage(n, k) {
+    const sorted = getSorted(true);
+    const index = sorted.length < n ? 0 : sorted.length - (n+1);
+    const lastDays = sorted.slice(index);
+    const reducer = (acc, day) => {
+      return acc + day[k];
+    }
+
+    return lastDays.reduce(reducer, 0);
   }
 
 };
