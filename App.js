46ad829004f4b700f58d0f70cf1b0175a5373335
Victor Lindhé <victorlindhe@gmail.com>
2018-04-11 20:24:39 +0200

    UI changes to tab bar

--- App.js
+++ App.js
@@ -4,15 +4,30 @@ import Settings from 'zendeff/components/settings';
 import Today from 'zendeff/components/today';
 import History from 'zendeff/components/history';
 import AddEntry from 'zendeff/components/addEntry';
+import Globals from 'zendeff/config/globals.js';
 
 /*
  * Application entry point.
  */
-const AppStack = TabNavigator({ 
-  Today: StackNavigator({ Today: Today }), 
-  History: StackNavigator({ History: History, AddEntry: AddEntry }),
-  Settings: StackNavigator({ Settings: Settings })
-});
+const AppStack = TabNavigator(
+  { 
+    Today: StackNavigator({ Today: Today }), 
+    History: StackNavigator({ History: History, AddEntry: AddEntry }),
+    Settings: StackNavigator({ Settings: Settings })
+  },
+  {
+    tabBarOptions: {
+      labelStyle: {
+        fontSize: 12,
+        color: '#fff',
+        marginBottom: 15
+      },
+      style: {
+        backgroundColor: Globals.PRIMARY_COLOR
+      }
+    }
+  }
+);
 
 export default SwitchNavigator(
   {
